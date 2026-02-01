9b5679fd209dbf639a08a84cf3e2f509fbfccc45
Surya079 <developer.surya03@gmail.com>
2026-02-01 17:31:48 +0530

    created rest login and validate token services endpoints

--- auth-service/src/main/java/com/pm/authservice/controller/AuthController.java
+++ auth-service/src/main/java/com/pm/authservice/controller/AuthController.java
@@ -1,4 +1,55 @@
 package com.pm.authservice.controller;
 
+import com.pm.authservice.dto.LoginRequestDto;
+import com.pm.authservice.dto.LoginResponseDto;
+import com.pm.authservice.service.AuthService;
+import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
 public class AuthController {
+
+    private final AuthService authService;
+
+    public AuthController(AuthService authService){
+        this.authService = authService;
+    }
+
+
+    @Operation(summary = "Generate a jwt token for auth service")
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponseDto> login(@RequestBody LoginRequestDto loginRequestDto){
+        Optional<String> tokenOptional = authService.authenticate(loginRequestDto);
+
+        if(tokenOptional.isEmpty()){
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+
+        String token = tokenOptional.get();
+        return ResponseEntity.ok(new LoginResponseDto(token));
+    }
+
+    @Operation(summary = "Validate token")
+    @GetMapping("/validate")
+    public ResponseEntity<Void> validateToken(
+            @RequestHeader("Authorization") String authHeader
+    ){
+
+        if(authHeader == null || !authHeader.startsWith("Bearer ")){
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+
+        }
+        if (!authHeader.substring(7).isEmpty()) {
+            String token = authHeader.substring(7);
+            return authService.validateToken(token)
+                    ? ResponseEntity.ok().build()
+                    : ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+
+        }
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+    }
 }
